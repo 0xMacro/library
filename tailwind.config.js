c8aba27c911d70b6cc40d87d6603ec3663caf25f
Gilbert <gilbertbgarza@gmail.com>
2021-10-12 13:44:28 -0500

    👀

--- tailwind.config.js
+++ tailwind.config.js
@@ -216,7 +216,7 @@ module.exports = {
         'ui': `ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`.split(', '),
         'sans': [`"GT-Walsheim-Regular"`, 'sans-serif'],
         'serif': `Constantia, "Lucida Bright", Lucidabright, "Lucida Serif", Lucida, "DejaVu Serif", "Bitstream Vera Serif", "Liberation Serif", Georgia, serif`.split(', '),
-        'mono': [`'Cutive Mono'`, ...defaultTheme.fontFamily.mono],
+        'mono-brand': [`'Cutive Mono'`, 'monospace'],
       },
 
       // https://github.com/tailwindlabs/tailwindcss-typography#customization
