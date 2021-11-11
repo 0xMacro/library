9945f8e45a8e7695cc33c2b4e5f07b563fc61b6e
Gilbert <gilbertbgarza@gmail.com>
2021-11-10 19:33:56 -0600

    Revert "Remove bias"
    This reverts commit 62bd44e22fae5f61841e33bd655e4117b80707f7.

---
+++ client/index.js
@@ -0,0 +1,7 @@
+console.log(
+`Welcome, developer.
+
+Hacker DAO is in the making.
+
+Find out more at https://hackerdao.xyz`
+)
--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  name: "TBD DAO",
+  name: "Hacker DAO",
   locales: ['en'],
 
   // These are to get @ethereumjs/vm working in the browser
